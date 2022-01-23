0e7b500d093d598391146de6c3a3f906c889d8dc
Shaiharyaar Ahmad <shaiharyaar1998@gmail.com>
2022-01-23 17:43:32 +0500

    animated 50% header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './style.scss'
+import gsap from 'gsap'
+import SplitText from '../../utils/Split3.min.js'
 
 const Navbar = () => {
+  useEffect(() => {
+    const split = new SplitText('#header-text-id', {
+      type: 'lines',
+      linesClass: 'lineChildren',
+    })
+
+    gsap.to(split.lines, {
+      duration: 1,
+      y: 0,
+      opacity: 1,
+      stagger: 0.1,
+      ease: 'power2',
+    })
+  }, [])
+
   return (
     <section className='header-container' data-scroll-section>
       <ul className='header-menu'>
@@ -9,7 +26,7 @@ const Navbar = () => {
         <li>About</li>
         <li>Featured</li>
       </ul>
-      <h1 id='header-text'>Art Objects</h1>
+      <h1 id='header-text-id'>Art Objects</h1>
     </section>
   )
 }
